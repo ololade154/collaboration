540bcb1421ad6839c9670e947ad07650309667d3
Ololade154 <shogbaikeoluwakemi2020@gmail.com>
2026-02-23 14:36:04 +0100

    update

---
+++ component.jsx
@@ -0,0 +1,10 @@
+export const Component = () => {
+  return (
+    <>
+      <h1>Hello</h1>
+      <div>Welcome,</div>
+      <h3>Shogbaike ololade</h3>
+      <h4>I am also welcome to the palace place, thanks </h4>
+    </>
+  );
+};
